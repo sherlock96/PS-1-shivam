143ea3921f42c5eafc27845832331250f3ed0062
sherlock96 <shivamagrawal3010@gmail.com>
2016-05-26 16:40:43 +0530

    Add files via upload

---
+++ WebApplication5/WebApplication5/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication5.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="width: 154px; height: 32px">
+    <form id="form1" runat="server">
+
+    <h1 style="height: 35px; width: 405px"> Welcome to the Login Page</h1>
+     <asp:Label ID="lblUserId" runat="server" Text="Login Id"></asp:Label>
+    <asp:TextBox ID="txtUserId" runat="server" Width="128px" 
+        Style="margin-top: 10px; margin-bottom: 10px; float: left" 
+        ></asp:TextBox>
+    <br />
+    
+       <p>
+         <asp:Label ID="lblPassword" runat="server" Text="Password"></asp:Label>  
+      </p>
+      <p>
+         <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"  
+            Width="128px"></asp:TextBox>
+       </p>
+    <asp:Button ID="Button1" runat="server"  Text="Register" OnClick="Button1_click" />
+    <asp:Label ID="lblStatus" runat="server" Text="" ></asp:Label>
+    </form>
+</body>
+</html>
---
+++ WebApplication5/WebApplication5/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_click(object sender, EventArgs e)
+        {
+            SqlConnection vid = new SqlConnection("Data Source=(local);Initial Catalog=master;User ID=sa; password='asdf@1234';");
+            
+            {
+                SqlCommand sc = new SqlCommand("Insert into database1(UserId,Password) Values(@UserId,@Password)", vid);
+                sc.Parameters.AddWithValue("@UserId", txtUserId.Text);
+                sc.Parameters.AddWithValue("@Password", txtPassword.Text);
+                vid.Open();
+                sc.ExecuteNonQuery();
+                vid.Close();
+
+                if (IsPostBack)
+                {
+                    lblStatus.Text = "Successfully added data";
+                    lblUserId.Text = "";
+                    txtPassword.Text = "";
+        }
+    }
+}
+
+       
+    }
+}
